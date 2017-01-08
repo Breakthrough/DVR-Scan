4e4032213cc11196513baba93b04a881aa3588b8
Breakthrough <brandon248@gmail.com>
2017-01-08 00:19:57 -0500

    Added get_seconds and get_timecode methods to FrameTimecode object, and completed class docstrings.

--- dvr_scan/timecode.py
+++ dvr_scan/timecode.py
@@ -75,6 +75,22 @@ def frame_to_timecode(frames, fps, show_msec = True):
 
 
 class FrameTimecode(object):
+    """ Object for frame-based timecodes, using the video framerate
+    to compute back and forth between frame number and second/timecode formats.
+
+    The passed argument is declared valid if it meets one of three valid forms:
+      1) Standard timecode HH:MM:SS[.nnn]:
+            in string form 'HH:MM:SS' or 'HH:MM:SS.nnn', or
+            in list/tuple form [HH, MM, SS] or [HH, MM, SS.nnn]
+      2) Number of seconds S[.SSS], where S >= 0.0:
+            in string form 'Ss' or 'S.SSSs' (e.g. '5s', '1.234s'), or
+            in integer or floating point form S or S.SSS
+      3) Exact number of frames N, where N >= 0:
+            in either integer or string form N or 'N'
+
+    Raises:
+        TypeError, ValueError
+    """
 
     def __init__(self, fps, timecode):
         if not isinstance(fps, (int, float)):
@@ -83,8 +99,8 @@ class FrameTimecode(object):
         self.frame_num = -1
         if isinstance(timecode, int):
             if timecode < 0:
-                raise ValueError('Timecode value must be positive.')
-            self.frame_num = int(timecode)
+                raise ValueError('Timecode frame number must be positive.')
+            self.frame_num = timecode
         elif isinstance(timecode, float):
             if timecode < 0.0:
                 raise ValueError('Timecode value must be positive.')
@@ -107,6 +123,11 @@ class FrameTimecode(object):
                 if secs < 0.0:
                     raise ValueError('Timecode seconds value must be positive.')
                 self.frame_num = int(secs * self.framerate)
+            elif timecode.isdigit():
+                timecode = int(timecode)
+                if timecode < 0:
+                    raise ValueError('Timecode frame number must be positive.')
+                self.frame_num = timecode
             else:
                 tc_val = timecode.split(':')
                 if not (len(tc_val) == 3 and tc_val[0].isdigit() and tc_val[1].isdigit()
@@ -122,10 +143,41 @@ class FrameTimecode(object):
         else:
             raise TypeError('Timecode format unrecognized.')
 
-    #def get_frame_num(self):
-    #    return self.frame_num
-
     def get_seconds(self):
+        """ Get the frame's position in number of seconds.
+
+        Returns:
+            A float of the current time/position in seconds.
+        """
         return float(self.frame_num) / self.framerate
+
+    def get_timecode(self, precision = 3, use_rounding = True):
+        """ Get a formatted timecode string of the form HH:MM:SS[.nnn].
+
+        Args:
+            precision:     The number of decimal places to include in the output [.nnn].
+            use_rounding:  True (default) to round the output to the desired precision.
+
+        Returns:
+            A string with a formatted timecode (HH:MM:SS[.nnn]).
+        """
+        # Compute hours and minutes based off of seconds, and update seconds.
+        secs = self.get_seconds()
+        base = 60.0 * 60.0
+        hrs = int(secs / base)
+        secs -= (hrs * base)
+        base = 60.0
+        mins = int(secs / base)
+        secs -= (mins * base)
+        # Convert seconds into string based on required precision.
+        if precision > 0:
+            if use_rounding:
+                secs = round(secs, precision)
+            msec = format(secs, '.%df' % precision)[-precision:]
+            secs = '%02d.%s' % (int(secs), msec)
+        else:
+            secs = '%02d' % int(round(secs, 0)) if use_rounding else '%02d' % int(secs)
+        # Return hours, minutes, and seconds as a formatted timecode string.
+        return '%02d:%02d:%s' % (hrs, mins, secs)
 
 
