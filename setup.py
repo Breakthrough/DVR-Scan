d85d2609437f5bafcdf9a19fda914406a55a089b
Brandon Castellano <brandon248@gmail.com>
2021-03-12 22:01:56 -0500

    Bump package version.

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ def get_extra_requires():
 
 setup(
     name='dvr-scan',
-    version='1.2',
+    version='1.2.1',
     description="Tool for finding and extracting motion events in video files"
                 "(e.g. security camera footage).",
     long_description=open('package-info.rst').read(),
