b48c0cc6f3af71c3e6f787d916e935d895dab86f
Brandon Castellano <brandon248@gmail.com>
2022-01-08 23:45:11 -0500

    Add new tests for pre/post event times.

--- tests/test_scan_context.py
+++ tests/test_scan_context.py
@@ -35,6 +35,7 @@ from dvr_scan.scanner import ScanContext
 
 # ROI within the frame used for the test case (see traffic_camera.txt for details).
 TRAFFIC_CAMERA_ROI = [631, 532, 210, 127]
+
 # Pairs of frames representing event start/end times.
 TRAFFIC_CAMERA_EVENTS = [
     (9, 148),
@@ -48,19 +49,33 @@ TRAFFIC_CAMERA_EVENTS_CNT = [
     (543, 575)
 ]
 
+# Pairs of frames representing event start/end times.
+TRAFFIC_CAMERA_EVENTS_TIME_PRE_5 = [
+    (4, 148),
+    (353, 490),
+    (537, 575)
+]
+
+# Pairs of frames representing event start/end times.
+TRAFFIC_CAMERA_EVENTS_TIME_POST_40 = [
+    (9, 138),
+    (358, 480),
+    (542, 575)  # Last event still ends on end of video.
+]
+
+
 def test_scan_context(traffic_camera_video):
     """ Test basic functionality of ScanContext with default parameters. """
 
     sctx = ScanContext([traffic_camera_video])
     sctx.set_detection_params(roi=TRAFFIC_CAMERA_ROI)
-    sctx.set_event_params(min_event_len=4, time_pre_event=0, time_post_event="2s")
+    sctx.set_event_params(min_event_len=4, time_pre_event=0)
 
     event_list = sctx.scan_motion()
 
     assert len(event_list) == len(TRAFFIC_CAMERA_EVENTS)
     # Remove duration, check start/end times.
-    event_list = [(event[0].frame_num, event[1].frame_num)
-                  for event in event_list]
+    event_list = [(event[0].frame_num, event[1].frame_num) for event in event_list]
     assert all([x == y for x, y in zip(event_list, TRAFFIC_CAMERA_EVENTS)])
 
 
@@ -69,12 +84,40 @@ def test_scan_context_cnt(traffic_camera_video):
 
     sctx = ScanContext([traffic_camera_video])
     sctx.set_detection_params(roi=TRAFFIC_CAMERA_ROI)
-    sctx.set_event_params(min_event_len=3, time_pre_event=0, time_post_event="2s")
+    sctx.set_event_params(min_event_len=3, time_pre_event=0)
 
     event_list = sctx.scan_motion(method='cnt')
 
     assert len(event_list) == len(TRAFFIC_CAMERA_EVENTS_CNT)
     # Remove duration, check start/end times.
-    event_list = [(event[0].frame_num, event[1].frame_num)
-                  for event in event_list]
+    event_list = [(event[0].frame_num, event[1].frame_num) for event in event_list]
     assert all([x == y for x, y in zip(event_list, TRAFFIC_CAMERA_EVENTS_CNT)])
+
+
+def test_pre_event_shift(traffic_camera_video):
+    """ Test setting time_pre_event. """
+
+    sctx = ScanContext([traffic_camera_video])
+    sctx.set_detection_params(roi=TRAFFIC_CAMERA_ROI)
+    sctx.set_event_params(min_event_len=4, time_pre_event=5)
+
+    event_list = sctx.scan_motion()
+
+    assert len(event_list) == len(TRAFFIC_CAMERA_EVENTS)
+    event_list = [(event[0].frame_num, event[1].frame_num) for event in event_list]
+    assert all([x == y for x, y in zip(event_list, TRAFFIC_CAMERA_EVENTS_TIME_PRE_5)])
+
+
+def test_post_event_shift(traffic_camera_video):
+    """ Test setting time_post_event. """
+
+    sctx = ScanContext([traffic_camera_video])
+    sctx.set_detection_params(roi=TRAFFIC_CAMERA_ROI)
+    sctx.set_event_params(min_event_len=4, time_pre_event=0, time_post_event=40)
+
+    event_list = sctx.scan_motion()
+
+    assert len(event_list) == len(TRAFFIC_CAMERA_EVENTS)
+    event_list = [(event[0].frame_num, event[1].frame_num) for event in event_list]
+    assert all([x == y for x, y in zip(event_list, TRAFFIC_CAMERA_EVENTS_TIME_POST_40)])
+
