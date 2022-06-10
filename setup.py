6439d9962c95d72331958a21b24aaedf9fae1601
Brandon Castellano <brandon248@gmail.com>
2022-06-10 19:47:16 -0400

    [build] Add back setup.py.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#      DVR-Scan: Video Motion Event Detection & Extraction Tool
+#   --------------------------------------------------------------
+#       [  Site: https://github.com/Breakthrough/DVR-Scan/   ]
+#       [  Documentation: http://dvr-scan.readthedocs.org/   ]
+#
+# Copyright (C) 2014-2022 Brandon Castellano <http://www.bcastell.com>.
+#
+""" DVR-Scan setup.py - DEPRECATED.
+
+Build using `python -m build` and install the resulting .whl using `pip`.
+"""
+
+from setuptools import setup
+setup()
