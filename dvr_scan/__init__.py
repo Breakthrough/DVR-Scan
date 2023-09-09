7d08037e20d2a5978f73e21d3920933b660ad901
Breakthrough <brandon248@gmail.com>
2023-09-08 23:25:21 -0400

    [build] Improve build matrix compatibility.

--- dvr_scan/__init__.py
+++ dvr_scan/__init__.py
@@ -45,7 +45,7 @@ from dvr_scan.platform import init_logger
 from dvr_scan.scanner import ScanContext
 
 # Used for module/distribution identification.
-__version__ = 'v1.5.1'
+__version__ = '1.5.1'
 
 
 def get_license_info() -> str:
