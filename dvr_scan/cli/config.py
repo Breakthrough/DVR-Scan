23dc72ac423d5d287c07842bf8b2d73111751986
Felix Scheffer <mail@felix-scheffer.de>
2024-05-09 01:20:46 +0200

    adds variance-threshold setting

--- dvr_scan/cli/config.py
+++ dvr_scan/cli/config.py
@@ -320,6 +320,7 @@ CONFIG_MAP: ConfigDict = {
     'bg-subtractor': 'MOG2',
     'threshold': 0.15,
     'max-threshold': 255.0,
+    'variance-threshold': 16.0,
     'kernel-size': KernelSizeValue(),
     'downscale-factor': 0,
     'learning-rate': float(-1),
