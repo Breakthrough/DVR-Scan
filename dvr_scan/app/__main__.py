b3288698c3812212fdd4c8685f4e576911f054f6
Breakthrough <brandon248@gmail.com>
2024-12-05 18:18:11 -0500

    [region-editor] Transition to Tcl/Tk for GUI #181
    This allows for better control over window events and makes input handling
    more consistent across platforms. It also allows better customization, for
    example, the system undo/redo commands are used instead of key binds.
    
    There is still much work to do, however this is a good starting point, and
    has feature parity with the existing editor.

---
+++ dvr_scan/app/__main__.py
@@ -0,0 +1,30 @@
+#
+#      DVR-Scan: Video Motion Event Detection & Extraction Tool
+#   --------------------------------------------------------------
+#       [  Site: https://www.dvr-scan.com/                 ]
+#       [  Repo: https://github.com/Breakthrough/DVR-Scan  ]
+#
+# Copyright (C) 2014-2024 Brandon Castellano <http://www.bcastell.com>.
+# DVR-Scan is licensed under the BSD 2-Clause License; see the included
+# LICENSE file, or visit one of the above pages for details.
+#
+"""``dvr_scan.app.__main__`` Module
+
+Provides entry point for DVR-Scan's graphical user interface (GUI).
+"""
+
+import sys
+
+
+def main():
+    """Main entry-point for DVR-Scan GUI."""
+    print("TODO.")
+
+    # For first pass just keep terminal window.
+    # Prompt for input file and just run DVR-Scan's main with changed argv.
+
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
