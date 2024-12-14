fe7dc7e6c8bbe49c6a53b998b63d22c0ce75ba32
Breakthrough <brandon248@gmail.com>
2024-12-13 22:39:22 -0500

    [dist] Include docs with source releases
    Remove images from source distributions to prevent binary bloat.
    
    Images are instead linked to the DVR-Scan website.

--- dist/pre_release.py
+++ dist/pre_release.py
@@ -80,7 +80,7 @@ VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
         )
 
 
-def build_docs():
+def build_docs(use_local_images=True):
     print("Building docs.")
 
     with TemporaryDirectory() as tmp:
@@ -96,6 +96,8 @@ def build_docs():
         curr_mkdocs_path = tmp.joinpath("docs", "mkdocs.yml")
         curr_mkdocs_path.replace(mkdocs_path)
 
+        docs_build_path = Path("dvr_scan").joinpath("docs")
+
         subprocess.run(
             [
                 "mkdocs",
@@ -103,12 +105,70 @@ def build_docs():
                 "--config-file",
                 mkdocs_path.absolute(),
                 "--site-dir",
-                Path("dvr_scan").joinpath("docs").absolute(),
+                docs_build_path.absolute(),
             ],
             check=True,
         )
+        print("Postprocessing docs.")
+
+        delete_and_replace = [
+            "assets/bounding-box.gif",
+            "assets/dvr-scan-logo.png",
+            "assets/dvr-scan.ico",
+            "assets/dvr-scan.png",
+            "assets/region-editor-mask.jpg",
+            "assets/region-editor-multiple.jpg",
+            "assets/region-editor-region.jpg",
+            "assets/region-editor-start.jpg",
+        ]
+
+        # Fix some issues with the output from mkdocs for local use without a web server.
+        # TODO: Should disable search feature on local docs since it doesn't work.
+        replacements = {'href="."': 'href="index.html"'}
+        if not use_local_images:
+            replacements.update(
+                {path: f"https://www.dvr-scan.com/{path}" for path in delete_and_replace}
+            )
+
+        for file in [Path(path) for path in docs_build_path.glob("**/**.html")]:
+            contents = file.read_text()
+            for old, new in replacements.items():
+                contents = contents.replace(old, new)
+            file.unlink(missing_ok=False)
+            file.write_text(contents)
+
+        if not use_local_images:
+            # Delete files now that they are not referenced.
+            for path in delete_and_replace:
+                docs_build_path.joinpath(path).unlink(missing_ok=False)
+
+        for to_remove in (
+            "sitemap.xml",
+            "sitemap.xml.gz",
+            "assets/images",
+            "assets/javascripts/lunr",
+            "assets/javascripts/workers",
+            "assets/javascripts/bundle.220ee61c.min.js.map",
+            "assets/stylesheets/main.eebd395e.min.css.map",
+            "assets/stylesheets/palette.ecc896b0.min.css.map",
+        ):
+            to_remove = docs_build_path.joinpath(to_remove)
+            shutil.rmtree(
+                to_remove, ignore_errors=False
+            ) if to_remove.is_dir() else to_remove.unlink()
+
+        def remove_mapping():
+            path = docs_build_path.joinpath("assets/javascripts/bundle.220ee61c.min.js")
+            contents = path.read_text()
+            TO_REMOVE = "//# sourceMappingURL=bundle.220ee61c.min.js.map\n"
+            assert TO_REMOVE in contents
+            contents = contents.replace(TO_REMOVE, "")
+            path.unlink()
+            path.write_text(contents)
+
+        remove_mapping()
 
 
 if __name__ == "__main__":
     write_version_info_for_windows_exe()
-    build_docs()
+    build_docs(use_local_images=bool(len(sys.argv) == 2 and sys.argv[1] == "--use-local-images"))
---
+++ docs/index_docs.md
@@ -0,0 +1,46 @@
+---
+hide:
+  - navigation
+  - toc
+---
+
+<img alt="DVR-Scan Logo" src="assets/dvr-scan.png" width="200rem"/>
+
+<h1 id="dvr-scan-title">DVR-Scan</h1>
+<h3 id="dvr-scan-subtitle">Find and extract motion events in videos.</h3>
+
+------------------------------------------------------
+
+!!! info "Version: 1.6.1 (May 18, 2024)"
+
+    <div class="buttongrid">[:fontawesome-solid-book: User Guide](guide.md){ .md-button #changelog-button }[:fontawesome-solid-bars: &nbsp; Documentation](docs.md){ .md-button #documentation-button }[:fontawesome-solid-gear: &nbsp; Resources](changelog.md){ .md-button #download-button }</div>
+
+------------------------------------------------------
+
+DVR-Scan is a command-line application that **automatically detects motion events in video files** (e.g. security camera footage).  DVR-Scan looks for areas in footage containing motion, and saves each event to a separate video clip.  DVR-Scan is free and open-source software, and works on Windows, Linux, and Mac.
+
+## :fontawesome-solid-person-running:Quickstart
+
+Scan `video.mp4` (separate clips for each event):
+
+    dvr-scan -i video.mp4
+
+Only scan a region of interest ([see user guide](guide.md#region-editor) or hit `H` for controls):
+
+    dvr-scan -i video.mp4 -r
+
+<img alt="overlay example" src="assets/region-editor-multiple.jpg" width="480"/>
+
+Draw boxes around motion:
+
+    dvr-scan -i video.mp4 -bb
+
+<img alt="overlay example" src="assets/bounding-box.gif" width="480"/>
+
+Use `ffmpeg` to extract events:
+
+    dvr-scan -i video.mp4 -m ffmpeg
+
+Once installed, see [the user guide](guide.md) to get started, try one of the examples above, or type `dvr-scan --help`. Press `Ctrl + C` to stop processing at any time.
+
+See the [documentation](docs.md) for a complete description of all [command-line](docs.md#dvr-scan-options) and [config file](docs.md#config-file) settings.
